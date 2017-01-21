dd9f2a8677f823dc296aea3fdc448ca7c01f149f
Amine Smahi <mohammed.amine.smahi@gmail.com>
2017-01-21 19:15:04 +0100

    added the main java to the hendller

--- src/com/company/FXMLController.java
+++ src/com/company/FXMLController.java
@@ -5,23 +5,144 @@
  */
 package com.company;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-/**
- * FXML Controller class
- *
- * @author DELL
- */
+
 public class FXMLController implements Initializable {
+  @FXML
+    private JFXButton submit;
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Label state;
+
+    @FXML
+    private JFXTextField answer;
+    @FXML
+    private Label word;
+    
+       int score = 0;
+
+    String[] dictionary1 = {"house", "beach", "heart", "children", "shower", "money", "luck"};
+    Word first = new Word(1, score, 0);
+    int h = -1;
+    
+    
+      public void creatingWord() {
+        Random d = new Random();
+
+        if (h != -1) {
+            for (int i = h; i < first.dictionary.size(); i++) {
+                first.dictionary.remove(i);
+            }
+        } else {
+            for (int i = 0; i < dictionary1.length; i++) {
+                first.dictionary.add(dictionary1[i]);
+            }
+        }
+
+        for (int i = 0; i < first.dictionary.size(); i++) {
+            System.out.println(first.dictionary.get(i));
+        }
+
+
+        first.wordLetters.clear();
+        first.organizedCharacters.clear();
+        h = d.nextInt(dictionary1.length);
+        first.testWord = first.dictionary.get(h);
+        first.generateWord(first.decompose(first.testWord), d.nextInt(2));
+
+        //make the textfield empty here
+        answer.setText("");
+
+       updateWord();
+
+    }
+
+    public void updateWord() {
+
+        for (int i = 0; i < first.organizedCharacters.size(); i++) {
+            //textfield receives getTextField + the new letter (previous + new)
+             word.setText(word.getText() + first.organizedCharacters.get(i));
+        }
+    }
+
+    // level two: i mean seriously guys, some graphics would be heaven now -_-
+    //i learned graphics old me..great job
+
+
+    public void guess() {
+        //just in case things go bad
+        //submit is the button
+
+        if (submit.getText() == "Check") {
+            //answer is the textfield
+            //checking if the textfield is empty
+            if (answer.getText().isEmpty()) {
+                //state is a label
+                state.setStyle("-fx-text-fill: #FFFFFF;-fx-alignment: center;");
+                state.setText("Writte at least one letter");
+            } else {
+                //in the guessingCharachter methode, insert the text in the textfield in lowercase
+                if (first.guessingCharachter(answer.getText().toLowerCase())) {
+
+                    if (first.organizedCharacters.equals(first.wordLetters)) {
+                       //next button is activated
+                        submit.setText("Next");
+                        //sound effects
+                       // music(3);
+                    } else {
+                       // music(1);
+                        //modifie the word, automaticaly
+                        updateWord();
+                    }
+                    //state is the label
+                    score = first.getScore();
+                    state.setStyle("-fx-text-fill: #00C853;-fx-alignment: center;");
+                    state.setText("Good Job, new Score: " + score);
+                } else {
+                    score = first.getScore();
+                    //state is the label
+                    state.setStyle("-fx-text-fill: #D50000;-fx-alignment: center;");
+                    state.setText("wrong, guess again !\nnew Score: " + score);
+                  //  music(2);
+                }
+                //answer is the textfield
+                answer.setText("");
+            }
+        } else {
+            if (first.dictionary.size() == 0) {
+                //state is the label
+                state.setStyle("fx-text-fill:#FFFFFF;");
+                state.setText("You Finished this Level");
+            } else {
+                creatingWord();
+                //submit is the button / check button gets activated
+                //submit.setEnabeled(true);
+            }
+        }
+
+    }
+    
+       @FXML
+    void icheck(ActionEvent event) {
+
+        if (event.getSource() == submit) {
+            guess();
+        }
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       
     }    
     
 }
